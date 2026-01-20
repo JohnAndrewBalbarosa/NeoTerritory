78aabd19002ad6b335f18c33bc562c840c02758e
JohnAndrewBalbarosa <202312632@fit.edu.ph>
2026-01-20 19:13:18 +0800

    Add initial SyntacticBrokenAST module and VSCode configs
    Introduces the SyntacticBrokenAST module with lexer, token, AST, utility, transform, and virtual node components. Adds main program and test for reading source, as well as VSCode configuration files for C++ development and build tasks.

---
+++ Project/Modules/SyntacticBrokenAST/token.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+enum class TokenType {
+    Preprocessor,
+    Identifier,
+    Keyword,
+    IntegerLiteral,
+    StringLiteral,
+    Operator,
+    Delimiter,
+    Error,
+    EndOfFile
+};
+
+struct Token {
+    TokenType type;
+    std::string lexeme;
+    std::string message;  // Only used when reporting errors.
+    size_t line;
+    size_t column;
+};
+
+std::string token_type_to_string(TokenType type);
---
+++ Project/Modules/SyntacticBrokenAST/transform.cpp
@@ -0,0 +1,11 @@
+#include "transform.hpp"
+
+VirtualNode* wrap_node(ASTNode* target, bool dirty) {
+    if (!target) {
+        return nullptr;
+    }
+    VirtualNode* wrapper = new VirtualNode(target, dirty);
+    wrapper->wrapper_line = target->line;
+    wrapper->wrapper_column = target->column;
+    return wrapper;
+}
